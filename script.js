96d7127010f425ad612c73e19ab634914937e430
AnjinIsmail <anjin.ismail@gmail.com>
2020-10-04 12:51:25 -0700

    added window.prompt criteria

--- script.js
+++ script.js
@@ -15,3 +15,9 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+var generatePassword =function() {
+  window.prompt("Your password must be between 8 and 128 characters.\n\nYour password must contain at least one uppercase, or capital, letter (ex: A, B, etc.).\n\nYour password must containt at least one lowercase letter.\n\nYour password must contain at least one number digit (ex:0, 1,2, etc).\n\nYour password must contain at least one special character (ex: $, %, *, etc) ")
+  
+  }; 
+  

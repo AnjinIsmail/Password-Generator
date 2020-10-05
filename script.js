2eb711347c26b4148132183e7015a77a145f1512
AnjinIsmail <anjin.ismail@gmail.com>
2020-10-04 18:57:42 -0700

    final

--- script.js
+++ script.js
@@ -9,70 +9,77 @@ function writePassword() {
   passwordText.value = password;
 }
 
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 var generatePassword = function () {
-  charactors = ""
-  passWord = ""
-  upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  lowerCase = "abcdefghijklmnopqrstuvwxyz"
-  Numerical = "0123456789"
-  specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}"
+  charactors = "";
+  passWord = "";
+  upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  Numerical = "0123456789";
+  specialCharacters = "!#$%&'()*+,-./:;<=>?@[]^_`{|}";
+  
+
+  window.alert(
+    "Password Criteria:\n\nMust be between 8 and 128 characters.\nMust contain at least one uppercase, or capital letter.\nMust containt at least one lowercase letter.\nMust contain at least one digit.\nMust contain at least one special character."
+  );
+
+  lengthCondition = true
+  while (lengthCondition) {
+    var myLength = parseInt(window.prompt(
+      "Please enter the length of the password you want to create:\n Note : Character lenght needs to be at least 8 to 128"
+    ));
+    if (myLength > 8 && myLength < 128){
+      lengthCondition = false
+    }
+    if (myLength < 8) {
+      window.alert(
+        "The minimum password length is 8. Please enter a valid number"
+      );
+      lengthCondition = true;
+    }
+    if (myLength > 128) {
+      window.alert(
+        "The maximum password length is 128. Please enter a valid number"
+      );
+      lengthCondition = true;
+    }
+  }
 
 
-  window.alert("Your password must be between 8 and 128 characters.\n\nYour password must contain at least one uppercase, or capital, letter (ex: A, B, etc.).\n\nYour password must containt at least one lowercase letter.\n\nYour password must contain at least one number digit (ex:0, 1,2, etc).\n\nYour password must contain at least one special character (ex: $, %, *, etc) ");
-
-
-  var myLength = window.prompt("Please enter the lenght of the password you want to create:\n Note : Character lenght needs to be at least 8 to 128")
-  if (myLength < 8 )
-    window.prompt("The minimum password length is 8. Please enter a valid number");
-
-
-
-  var upperconfirm = window.confirm("Do you want Upper case?")
+  var upperconfirm = window.confirm("Do you want Upper case?");
   if (upperconfirm) {
-    charactors = charactors + upperCase
-    console.log(charactors)
-  }
-  else {
-    charactors = charactors + ""
+    charactors = charactors + upperCase;
+  } else {
+    charactors = charactors + "";
   }
 
-  var lowerconfirm = window.confirm("Do you want Lower case?")
+  var lowerconfirm = window.confirm("Do you want Lower case?");
   if (lowerconfirm) {
-    charactors = charactors + lowerCase
-    console.log(charactors)
-  }
-  else {
-    charactors = charactors + ""
+    charactors = charactors + lowerCase;
+  } else {
+    charactors = charactors + "";
   }
 
-  var Numericalconfirm = window.confirm("Do you want Numbers?")
+  var Numericalconfirm = window.confirm("Do you want Numbers?");
   if (Numericalconfirm) {
-    charactors = charactors + Numerical
-    console.log(charactors)
-  }
-  else {
-    charactors = charactors + ""
+    charactors = charactors + Numerical;
+  } else {
+    charactors = charactors + "";
   }
 
-
-  var specialCharactersconfirm = window.confirm("Do you want Special characters?")
+  var specialCharactersconfirm = window.confirm(
+    "Do you want Special characters?"
+  );
   if (specialCharactersconfirm) {
-    charactors = charactors + specialCharacters
-    console.log(charactors)
-
+    charactors = charactors + specialCharacters;
   }
-
 
   for (i = 0; i < myLength; i++) {
     generate = Math.floor(Math.random() * charactors.length + 1);
-    passWord = passWord + charactors.charAt(generate)
-
+    passWord = passWord + charactors.charAt(generate);
   }
 
-  console.log(passWord);
-  return passWord
+  return passWord;
 }
